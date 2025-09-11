fbf9cfc9b3f247f8b1ac386ec4c16e5df31f5725
wesleyfernd <wesley2030@icloud.com>
2025-09-11 19:58:47 -0300

    R4 - Login.go

--- R4 - Login.go
+++ R4 - Login.go
@@ -4,15 +4,29 @@ import (
 	"fmt"
 )
 
-func main() {
-	var numero int
-	fmt.Print("Digite um número: ")
-	fmt.Scan(&numero)
+func verificarLogin(usuario, senha string) bool {
+	if usuario == "admin" && senha == "senha" {
+		return true
+	} else {
+		return false
+	}
+}
 
-	switch numero % 2 {
-	case 0:
-		fmt.Println("O número é par")
-	default:
-		fmt.Println("O número é ímpar")
+func main() {
+	for {
+		var usuario, senha string
+
+		fmt.Print("Digite o nome de usuário: ")
+		fmt.Scan(&usuario)
+
+		fmt.Print("Digite a senha: ")
+		fmt.Scan(&senha)
+
+		if verificarLogin(usuario, senha) {
+			fmt.Println("Login bem-sucedido!")
+			break
+		} else {
+			fmt.Println("Usuário ou senha incorretos. Tente novamente.")
+		}
 	}
 }
