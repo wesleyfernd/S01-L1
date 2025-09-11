cc83888ab0f384d119d75ef6e315a272adcdd253
wesleyfernd <wesley2030@icloud.com>
2025-09-11 19:58:14 -0300

    R4 - Fibonacci.go

--- R4 - Fibonacci.go
+++ R4 - Fibonacci.go
@@ -4,15 +4,20 @@ import (
 	"fmt"
 )
 
-func main() {
-	var numero int
-	fmt.Print("Digite um número: ")
-	fmt.Scan(&numero)
-
-	switch numero % 2 {
-	case 0:
-		fmt.Println("O número é par")
-	default:
-		fmt.Println("O número é ímpar")
+func fibonacci(n int) {
+	a, b := 0, 1
+	for i := 0; i < n; i++ {
+		fmt.Print(a, " ")
+		a, b = b, a+b
 	}
+	fmt.Println()
+}
+
+func main() {
+	var n int
+	fmt.Print("Digite quantos números da sequência de Fibonacci você deseja: ")
+	fmt.Scan(&n)
+
+	fmt.Println("Sequência de Fibonacci:")
+	fibonacci(n)
 }
